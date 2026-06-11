29a25a131b8ee19aafc19eff89d4f4db03e7624d
Josue <josue.gil@meetingdoctors.com>
2026-06-11 10:01:01 +0200

    feat: new serializer contract

--- tests/SerializerTest.php
+++ tests/SerializerTest.php
@@ -7,39 +7,50 @@ namespace Serializer\Tests;
 use PHPUnit\Framework\TestCase;
 use Serializer\SerializableInterface;
 use Serializer\Serializer;
-use Serializer\SerializerException;
 
 final class SerializerTest extends TestCase
 {
-    public function test_not_must_deserialize_array_when_not_implementing_serializable(): void
+    public function test_must_deserialize_serializable_object(): void
     {
-        self::expectException(SerializerException::class);
-
-        Serializer::deserialize(['class' => 'stdClass', 'attributes' => ['foo' => 'bar']]);
-    }
-
-    public function test_must_deserialize_array_when_implementing_serializable(): void
-    {
-        $serializable = Serializer::deserialize(['class' => Serializable::class, 'attributes' => []]);
-
-        self::assertInstanceOf(Serializable::class, $serializable);
-    }
-
-    public function test_must_serialize_array_when_implementing_serializable(): void
-    {
-        $serializable = Serializer::serialize(new Serializable());
-
-        self::assertSame([
+        $serializable = Serializer::deserialize([
             'class' => Serializable::class,
             'attributes' => [],
-        ], $serializable);
+        ]);
+
+        self::assertInstanceOf(
+            Serializable::class,
+            $serializable,
+        );
+    }
+
+    public function test_must_serialize_serializable_object(): void
+    {
+        self::assertSame(
+            [
+                'class' => Serializable::class,
+                'attributes' => [],
+            ],
+            Serializer::serialize(new Serializable()),
+        );
+    }
+
+    public function test_must_restore_serialized_object(): void
+    {
+        $object = new Serializable();
+
+        self::assertEquals(
+            $object,
+            Serializer::deserialize(
+                Serializer::serialize($object),
+            ),
+        );
     }
 }
 
 final readonly class Serializable implements SerializableInterface
 {
     #[\Override]
-    public static function deserialize(array $data): self
+    public static function deserialize(array $attributes): static
     {
         return new self();
     }
